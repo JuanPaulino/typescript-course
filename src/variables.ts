40a1d0b16e629cff33bd67826753f5826e97b11d
Juan Francisco Paulino Veras <juan.paulino.veras@gmail.com>
2021-01-10 13:04:41 +0100

    fix: adjustment of the nomenclature in the constants

--- src/variables.ts
+++ src/variables.ts
@@ -1,9 +1,9 @@
 module Variables {
   var userName = 'Juan Paulino';
   let address = 'Calle Federico, 6, 2034, Madrid';
-  const NumberOfSubscribers = 1000;
+  const NUMBER_OF_SUBSCRIBERS = 1000;
 
   console.log(userName);
   console.log(address);
-  console.log(NumberOfSubscribers);
+  console.log(NUMBER_OF_SUBSCRIBERS);
 }
