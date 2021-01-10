f6148955b303ecb64786ec61d8b6d673b9482d20
Juan Francisco Paulino Veras <juan.paulino.veras@gmail.com>
2021-01-10 13:00:04 +0100

    feat: example for the exercise of variables

---
+++ src/variables.ts
@@ -0,0 +1,9 @@
+module Variables {
+  var userName = 'Juan Paulino';
+  let address = 'Calle Federico, 6, 2034, Madrid';
+  const NumberOfSubscribers = 1000;
+
+  console.log(userName);
+  console.log(address);
+  console.log(NumberOfSubscribers);
+}
